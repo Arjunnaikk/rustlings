953b05021e1c5a182e1a65356d8c3d36baa99440
Arjun <arjunnaik0402@gmail.com>
2025-11-09 12:14:15 +0000

    Update

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,7 +1,10 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
-
+    let a = [1; 100];
+    // for i in a {
+    //     let e = a[i];
+    //     println!("{e}");
+    // }
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
